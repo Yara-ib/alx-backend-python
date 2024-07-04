dfe1a96cb8e60876b62c3f689daf9b9633227008
Yara-ib <yr.ap.23@gmail.com>
2024-07-04 14:47:00 +0300

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+" Basic Annotations >> Add Module "
+
+
+def add(a: float, b: float) -> float:
+    """
+    Sum of two variables
+    Args:
+        a: float
+        b: float
+    Return:
+        Sum of the two variables
+    """
+    return a + b

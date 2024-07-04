41af7fe94c73eb7ae4954e2028e364afebe596c5
Yara-ib <yr.ap.23@gmail.com>
2024-07-04 15:02:34 +0300

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import math
+" Basic Annotations >> Floor Module "
+
+
+def floor(n: float) -> int:
+    """
+    Args:
+        n: float
+    Return:
+        Floor of the float
+    """
+    return math.floor(n)

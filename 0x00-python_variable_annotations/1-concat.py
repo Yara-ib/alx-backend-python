d58fe0e72360c7deabb200f6f0a653ba0a5899f5
Yara-ib <yr.ap.23@gmail.com>
2024-07-04 14:56:04 +0300

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+" Basic Annotations >> Concat Module "
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenation of two variables
+    Args:
+        str1: string
+        str2: string
+    Return:
+        Concatenated string
+    """
+    return str1 + str2

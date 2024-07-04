a521817e0cbf5ec4928b4f7e9578ca2440a2d0e2
Yara-ib <yr.ap.23@gmail.com>
2024-07-04 15:10:34 +0300

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+" Basic Annotations >> To String Module "
+
+
+def to_str(n: float) -> str:
+    """
+    Args:
+        n: float
+    Return:
+        String representation of the float
+    """
+    return n.__str__()

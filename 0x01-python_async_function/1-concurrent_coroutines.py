1ad39f835c5a840e12eb904fb9377bac85e07c43
Yara-ib <yr.ap.23@gmail.com>
2024-07-09 03:41:23 +0300

    Multiple Coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -24,4 +24,4 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     list_delays = []
     for task in completed:
         list_delays.append(task.result())
-    return sorted(list_delays)
+    return list_delays

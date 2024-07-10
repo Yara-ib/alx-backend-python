6e5df1e0e29c0bcbaa7a81a03121bb8014c328ad
Yara-ib <yr.ap.23@gmail.com>
2024-07-10 03:04:52 +0300

    Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
 """ Async Comprehensions Module """
-import asyncio
-import random
-from typing import Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """
     Coroutine collect 10 random numbers using an async comprehension
     over async_generator, then return the 10 random numbers
 
     Returns:
-        Generator[float, None, None]: The value of the delays
-
-    Yields:
-        Iterator[Generator[float, None, None]]: Yields random delays in a list
+        List[float]: The list of 10 random numbers
     """
     return [i async for i in async_generator()]
